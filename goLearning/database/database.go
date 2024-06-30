5a7b39b49058b76399fadfb7b7343990fec724f0
sipatir08 <114630694+sipatir08@users.noreply.github.com>
2024-06-30 16:12:26 +0700

    Update database.go

--- goLearning/database/database.go
+++ goLearning/database/database.go
@@ -11,7 +11,7 @@ var DB *sql.DB
 
 func InitDB() {
     var err error
-    connStr := "root@tcp(localhost:3306)/go_learning"
+    connStr := "root:p3ws@tcp(localhost:3306)/go_learning"
     DB, err = sql.Open("mysql", connStr)
     if err != nil {
         log.Fatal(err)
